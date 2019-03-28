26e26a73d379917721f735c7453083368b30b038
guenther <email@suepergue.de>
2019-03-28 23:15:37 +0100

    adds some example tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import App from './App';
-import HelloWorld from './components/shared/HelloWorld';
+import Form from './components/shared/Form';
+import Todo from './components/shared/Todo';
 
 describe('<App />', () => {
   const wrap = mount(<App />);
@@ -11,7 +12,18 @@ describe('<App />', () => {
     expect(wrap.find(App).exists()).toBe(true);
   });
 
-  it('contains HelloWorld component', () => {
-    expect(wrap.find(HelloWorld).exists()).toBe(true);
+  it('contains Form component', () => {
+    expect(wrap.find(Form).exists()).toBe(true);
+  });
+
+  it('contains Todo component', () => {
+    const input = wrap.find('input');
+    const form = wrap.find(Form);
+
+    input.simulate('change', { target: { value: 'test-todo-item' } });
+    form.simulate('submit');
+
+    expect(wrap.find(Todo).exists()).toBe(true);
+    expect(wrap.find(Todo).text()).toBe('test-todo-item');
   });
 });
